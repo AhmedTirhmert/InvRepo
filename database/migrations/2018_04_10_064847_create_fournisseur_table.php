78189b9ec31e15f2b3aa921cf5d572c79d65e82a
AhmedTirhmert <ahmedtoto301@gmail.com>
2018-04-10 12:56:59 +0100

    migration correction

--- database/migrations/2018_04_10_064847_create_fournisseur_table.php
+++ database/migrations/2018_04_10_064847_create_fournisseur_table.php
@@ -14,22 +14,13 @@ class CreateFournisseurTable extends Migration
     public function up()
     {
         Schema::create('fournisseur', function (Blueprint $table) {
-<<<<<<< HEAD:database/migrations/2018_04_10_064847_create_fournisseur_table.php
+
             $table->integer('code_f')->primary();
             $table->string('nom',10);
             $table->string('prenom',10);
             $table->string('Adresse',14);
             $table->string('telephone',14)->unique();
             $table->string('email',20)->unique();
-
-=======
-            $table->integer('Code_F')->autoIncrement();
-            $table->string('Prenom');
-            $table->string('Nom');
-            $table->string('Adresse');
-            $table->string('Telephone')->unique();
-            $table->string('Email')->unique();
->>>>>>> 6697e12af02141b81109ea3bac2367a6a36c97b4:database/migrations/2018_04_09_231122_create__fournisseur_table.php
             $table->timestamps();
         });
     }
