2822409a244894c8399191548a0a64a13b3a9b4c
RAHMA_MA <maissou.rahma@gmail.com>
2018-04-16 11:20:44 +0200

    commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
+        $this->call(EtatcommandeTableSeeder::class);
+        $this->call(TypeutilisateurTableSeeder::class);
+        $this->call(CategorieTableSeeder::class);
+        $this->call(AdminsTableSeeder::class);
+        $this->call(UtilisateurTableSeeder::class);
+        $this->call(ProduitTableSeeder::class);
+        $this->call(CommandeTableSeeder::class);
+        $this->call(ConcerneTableSeeder::class);
    
     }
 }
